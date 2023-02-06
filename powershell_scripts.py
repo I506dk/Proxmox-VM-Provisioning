16ddb9e926914c3fe6ee7d912c134f100239ed3d
I506dk <33561466+I506dk@users.noreply.github.com>
2023-02-06 11:23:58 -0600

    Update powershell_scripts.py

--- powershell_scripts.py
+++ powershell_scripts.py
@@ -58,8 +58,6 @@ def install_7zip():
     command = """# Modern websites require TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $dlurl = "https://7-zip.org/" + (Invoke-WebRequest -UseBasicParsing -Uri "https://7-zip.org/" | Select-Object -ExpandProperty Links | Where-Object {($_.outerHTML -match "Download")-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
-# modified to work without IE
-# above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
 $installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
 Invoke-WebRequest $dlurl -OutFile $installerPath
 Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
