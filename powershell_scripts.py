ea9758f2bddbdb35ecf898399d3059eae57240b4
I506dk <33561466+I506dk@users.noreply.github.com>
2023-02-08 21:14:14 -0600

    Update powershell_scripts.py

--- powershell_scripts.py
+++ powershell_scripts.py
@@ -72,10 +72,11 @@ def install_python():
     script_name = "install_python.ps1"
     # Command to install the latest python version
     command = """# Modern websites require TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$latest_url = (Invoke-RestMethod "https://www.python.org/downloads/") -match "\\bhref=(?<url>.+?\.exe)"
+[[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    
+$latest_url = (Invoke-RestMethod "https://www.python.org/downloads/") -match "\bhref=(?<url>.+?\.exe)"
 $latest_url = $Matches.url
-$latest_url = $latest_url.replace("`"", "")
+$latest_url = $latest_url.replace('"', "")
 
 $latest_version = (Invoke-RestMethod "https://www.python.org/downloads/") -match "Download Python (?<version>\d+\.\d+\.\d+)"
 $latest_version = $Matches.version
@@ -84,8 +85,13 @@ Write-Host "Installing Python version:" $latest_version
 $installerPath = Join-Path $env:TEMP (Split-Path $latest_url -Leaf)
 
 Invoke-WebRequest $latest_url -OutFile $installerPath
-Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
-Remove-Item $installerPath"""
+Start-Process -FilePath $installerPath -Args "/quiet InstallAllUsers=1 PrependPath=1" -Verb RunAs -Wait
+Remove-Item $installerPath
+
+# Add python and pip to PATH
+$executable_path = py -$latest_version -c "import sys; print(sys.executable[:-10])"
+$scripts_path = $executable_path + "Scripts\"
+setx path "%path%;$executable_path;$scripts_path"""
     
     return command, script_name
     
